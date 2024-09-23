80573ae67617ab2068dfedef9de88429532430b5
parth23571 <pawal.sateesh@gmail.com>
2024-09-23 14:33:44 +0530

    Satis

--- src/main/java/SeleniumTest/Test1/POMSearch.java
+++ src/main/java/SeleniumTest/Test1/POMSearch.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 //10:30 10:42 - 12
 //10.:50 10:59 - 09
 //11:24 11:30 - 06 >> 27 min
+//My Satish - 14:33 pm
 public class POMSearch {
 	
 	public WebDriver driver=null;
